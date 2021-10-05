87a4df915d14e97a61ab15d2b6dca390b7041f86
Rachel Lewis <RLewis11769@gmail.com>
2021-10-05 23:58:59 +0000

    Add data to 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts node as left child
+ * @parent: pointer to parent node that contains left child
+ * @value: value to store in new left child
+ * Return: pointer to new node
+ */
+
+/* If parent already has left child, replace with new node */
+/* Old left child is left child of new node */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (parent->n < value)
+	{
+		new_node->n = parent->n;
+		parent->n = value;
+	}
+	else
+		new_node->n = value;
+
+	if (parent->left)
+	{
+		if (new_node->n > parent->left->n)
+		{
+			new_node->n = parent->left->n;
+			parent->left->n = value;
+		}
+		parent->left->left = new_node;
+		new_node->parent = parent->left;
+	}
+	else
+	{
+		parent->left = new_node;
+		new_node->parent = parent->left;
+	}
+
+	return (new_node);
+}
