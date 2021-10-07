2de62c9170bc08302f688d1a334acd573c166b98
Rachel Lewis <RLewis11769@gmail.com>
2021-10-07 02:17:42 +0000

    Simplify - remove if statements

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,21 +24,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	if (parent->n < value)
-	{
-		new_node->n = parent->n;
-		parent->n = value;
-	}
-	else
-		new_node->n = value;
-
 	if (parent->left)
 	{
-		if (new_node->n > parent->left->n)
-		{
-			new_node->n = parent->left->n;
-			parent->left->n = value;
-		}
+		new_node->n = parent->left->n;
+		parent->left->n = value;
 		parent->left->left = new_node;
 		new_node->parent = parent->left;
 	}
@@ -46,6 +35,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		parent->left = new_node;
 		new_node->parent = parent;
+		new_node->n = value;
 	}
 
 	return (new_node);
